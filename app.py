c7c293168b962bee3b54fc57ff8b8a15eb300ca8
Karlets-i-mes <karltotimes@gmail.com>
2025-03-25 10:37:12 +0000

    Pequeño codigo flet

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-print("pringao")
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Ejemplo de Flet"
+
+    def boton_click(e):
+        texto.value = "¡Botón clickeado!"
+        page.update()
+
+    texto = ft.Text("Haz clic en el botón")
+    boton = ft.ElevatedButton("Clic aquí", on_click=boton_click)
+
+    page.add(
+        ft.Column([
+            texto,
+            boton,
+        ])
+    )
+
+ft.app(target=main, view=ft.WEB_BROWSER)
